e7001392559832ded130730cf912d40e643e6255
TheFizzWare <AustinStarnes.136@gmail.com>
2020-05-08 07:53:21 -0400

    1.1.0 Update  - Restructured Block Registration Code  - Cleanup Code  - Updated Jungle and Acacia door textures to have cutout/transparent parts (more like vanilla)  - Updated Block Models so that the middle block of a tall door doesn't render the upwards face  - Added parent models for middle door block

--- src/main/java/com/fizzware/dramaticdoors/blocks/DramaticDoorsBlocks.java
+++ src/main/java/com/fizzware/dramaticdoors/blocks/DramaticDoorsBlocks.java
@@ -9,29 +9,40 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ObjectHolder;
 
+
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 @ObjectHolder(DramaticDoors.MOD_ID)
 public class DramaticDoorsBlocks {
 
-    @ObjectHolder(TallDoorBlock.NAME_OAK) public static final Block TALL_OAK_DOOR = null;
-    @ObjectHolder(TallDoorBlock.NAME_SPRUCE) public static final Block TALL_SPRUCE_DOOR = null;
-    @ObjectHolder(TallDoorBlock.NAME_BIRCH) public static final Block TALL_BIRCH_DOOR = null;
-    @ObjectHolder(TallDoorBlock.NAME_JUNGLE) public static final Block TALL_JUNGLE_DOOR = null;
-    @ObjectHolder(TallDoorBlock.NAME_ACACIA) public static final Block TALL_ACACIA_DOOR = null;
-    @ObjectHolder(TallDoorBlock.NAME_DARK_OAK) public static final Block TALL_DARK_OAK_DOOR = null;
-    @ObjectHolder(TallDoorBlock.NAME_IRON) public static final Block TALL_IRON_DOOR = null;
+    public static Block TALL_OAK_DOOR;
+    public static Block TALL_SPRUCE_DOOR;
+    public static Block TALL_BIRCH_DOOR;
+    public static Block TALL_JUNGLE_DOOR;
+    public static Block TALL_ACACIA_DOOR;
+    public static Block TALL_DARK_OAK_DOOR;
+    public static Block TALL_IRON_DOOR;
 
     @SubscribeEvent
-    public static void registerBlocks(final RegistryEvent.Register<Block> blockRegistry) {
+    public static void registerBlocks(final RegistryEvent.Register<Block> blockRegistry)
+    {
+        TALL_OAK_DOOR = new TallDoorBlock(Blocks.OAK_DOOR);
+        TALL_SPRUCE_DOOR = new TallDoorBlock(Blocks.SPRUCE_DOOR);
+        TALL_BIRCH_DOOR = new TallDoorBlock(Blocks.BIRCH_DOOR);
+        TALL_JUNGLE_DOOR = new TallDoorBlock(Blocks.JUNGLE_DOOR);
+        TALL_ACACIA_DOOR = new TallDoorBlock(Blocks.ACACIA_DOOR);
+        TALL_DARK_OAK_DOOR = new TallDoorBlock(Blocks.DARK_OAK_DOOR);
+        TALL_IRON_DOOR = new TallDoorBlock(Blocks.IRON_DOOR);
+
         blockRegistry.getRegistry().registerAll(
-                new TallDoorBlock(Blocks.OAK_DOOR).setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_OAK)),
-                new TallDoorBlock(Blocks.SPRUCE_DOOR).setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_SPRUCE)),
-                new TallDoorBlock(Blocks.BIRCH_DOOR).setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_BIRCH)),
-                new TallDoorBlock(Blocks.JUNGLE_DOOR).setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_JUNGLE)),
-                new TallDoorBlock(Blocks.ACACIA_DOOR).setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_ACACIA)),
-                new TallDoorBlock(Blocks.DARK_OAK_DOOR).setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_DARK_OAK)),
-                new TallDoorBlock(Blocks.IRON_DOOR).setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_IRON))
+                TALL_OAK_DOOR.setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_OAK)),
+                TALL_SPRUCE_DOOR.setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_SPRUCE)),
+                TALL_BIRCH_DOOR.setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_BIRCH)),
+                TALL_JUNGLE_DOOR.setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_JUNGLE)),
+                TALL_ACACIA_DOOR.setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_ACACIA)),
+                TALL_DARK_OAK_DOOR.setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_DARK_OAK)),
+                TALL_IRON_DOOR.setRegistryName(new ResourceLocation(DramaticDoors.MOD_ID, TallDoorBlock.NAME_IRON))
         );
+
     }
 
 }
